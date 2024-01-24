1242722d25694e39af6e348446a9e25eda5dd4d8
ruttklein <klein.rutto@student.moringaschool.com>
2024-01-24 23:49:49 +0300

    updated login and logout

---
+++ deli-med/src/logout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+const Logout = () => {
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/logout');
+
+      console.log(response.data.message);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Logout</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Logout;

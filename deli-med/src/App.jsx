71b4fa5aea81764489d73f4f321b4c8335970c65
Joyce Mwangi <105669036+JOYCEmwangi8880@users.noreply.github.com>
2024-01-24 21:38:12 -0100

    front-end

---
+++ deli-med/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      
+      </div>
+      <h1>Welcone to DELI-MED pharmacy</h1>
+     
+      <p className="read-the-docs">
+        Where you get to feel better!!!
+      </p>
+    </>
+  )
+}
+
+export default App
